09d751facabd5328b2e48c80dafb7b21ae416e9e
iamsatyanand <iamsatyanandmishra@gmail.com>
2026-03-07 23:03:15 +0530

    initial project set up done

---
+++ src/main/java/com/satyanand/distributedwallet/DistributedWalletApplication.java
@@ -0,0 +1,13 @@
+package com.satyanand.distributedwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/satyanand/distributedwallet/DistributedWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satyanand.distributedwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
